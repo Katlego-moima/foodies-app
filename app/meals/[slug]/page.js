2a6a3ef5df4e4c8739871e17dce94a258239de66
Katlego <moimakatlego@outlook.com>
2024-04-11 00:46:38 +0200

    created routes and dynamic routes also initialized an sqlite db

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,42 @@
+import React from "react";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export default function MealDetailPage({ params }) {
+  console.log("params.mealSlug:", params.slug);
+  const meal = getMeal(params.slug);
+  console.log("meal:", meal);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+}
